2222fd97272df3d0d1c334bb6f5918225f0720b4
ccatalfo <foxnorth@gmail.com>
2014-03-16 16:45:14 -0400

    Add basic test file which checks number of fields in parsed record.

--- test/Test.hs
+++ test/Test.hs
@@ -1,11 +1,39 @@
 module Main where
 
-import Test.Tasty (defaultMain,testGroup,TestTree)
+import Test.Tasty (defaultMain,testGroup,TestTree,withResource)
+import Test.Tasty.HUnit
+import Marc
+import System.FilePath.Posix
+import System.Environment
+import Paths_marc
+
+
+data MarcFileResource = MarcFileResource String
+
+acquire :: IO MarcFileResource
+acquire = do
+        file <- getDataFileName "data/record.mrc"
+        s <- readFile file
+        return $ MarcFileResource s
+
+release :: MarcFileResource -> IO ()
+release resource = return ()
+
+testWithMarcFileResource :: MarcFileResource -> Assertion
+testWithMarcFileResource (MarcFileResource marcString) =
+       let record = readFromString marcString in
+    assertEqual "should have correct number of fields" 20 (length $ mrFields record)
+
 
 main :: IO ()
-main = defaultMain tests
+main = defaultMain $
+     withResource acquire release tests
 
-tests :: TestTree
-tests = testGroup "All Tests"
-            [
-            ]
+tests :: IO MarcFileResource -> TestTree
+tests resource = testGroup "All Tests"
+                     [
+                     testGroup "Unit Tests"
+                           [ testCase "Correct number of fields found" $ resource >>= testWithMarcFileResource
+
+                            ]
+                     ]
