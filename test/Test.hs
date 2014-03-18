ef0ca782cad14258450f3c4740b949ea006ab7f5
ccatalfo <foxnorth@gmail.com>
2014-03-17 21:10:12 -0400

    Fix marc control field check.

--- test/Test.hs
+++ test/Test.hs
@@ -19,10 +19,15 @@ acquire = do
 release :: MarcFileResource -> IO ()
 release resource = return ()
 
-testWithMarcFileResource :: MarcFileResource -> Assertion
-testWithMarcFileResource (MarcFileResource marcString) =
-       let record = readFromString marcString in
-    assertEqual "should have correct number of fields" 20 (length $ mrFields record)
+testNumberOfFields :: MarcFileResource -> Assertion
+testNumberOfFields (MarcFileResource marcString) =
+                       let record = readFromString marcString in
+                    assertEqual "should have correct number of fields" 20 (length $ mrFields record)
+
+testNumberOfControlFields :: MarcFileResource -> Assertion
+testNumberOfControlFields (MarcFileResource marcString) =
+                          let record = readFromString marcString
+                          in assertEqual "it should have correct number of control fields" 4 (length $ getControlFields record)
 
 
 main :: IO ()
@@ -33,7 +38,8 @@ tests :: IO MarcFileResource -> TestTree
 tests resource = testGroup "All Tests"
                      [
                      testGroup "Unit Tests"
-                           [ testCase "Correct number of fields found" $ resource >>= testWithMarcFileResource
+                           [ testCase "Correct number of fields found" $ resource >>= testNumberOfFields
+                             ,testCase "Correct number of control fields found" $ resource >>= testNumberOfControlFields
 
                             ]
                      ]
