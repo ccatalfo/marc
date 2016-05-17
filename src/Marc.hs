c72be053ac50d51c225d35815cc426756fcd8cf9
Chris Catalfo <foxnorth@gmail.com>
2016-05-16 21:28:33 -0400

    Add some initial documentation.

--- src/Marc.hs
+++ src/Marc.hs
@@ -21,6 +21,8 @@ marc21DirectoryLength = 12
 type Marc21Leader = String
 type Marc21BaseAddressOfData = Int
 
+-- |Represents the MARC21 Directory entry (see: https://www.loc.gov/marc/bibliographic/bddirectory.html)
+-- Contains a tag, length of field  and starting position in the binary MARC file.
 data Marc21DirEntry = Marc21DirEntry {
       mdTag              :: Marc21Tag
       , mdLengthofField  :: Int
@@ -29,6 +31,7 @@ data Marc21DirEntry = Marc21DirEntry {
 instance Show Marc21DirEntry where
     show (Marc21DirEntry mdTag mdLengthofField mdStartPosition) = mdTag ++ " " ++ (show mdLengthofField) ++ " " ++ (show mdStartPosition) ++ "\n"
 
+-- |Represents the MARC21 Directory (see: https://www.loc.gov/marc/bibliographic/bddirectory.html)
 data Marc21Directory = Marc21Directory {
       mdEntries          :: [Marc21DirEntry]
 } deriving (Show)
@@ -37,6 +40,7 @@ type Marc21TagCode = String
 type Marc21SubfieldCode = Char
 type Marc21SubfieldValue = String
 
+-- |Represents a MARC21 subfield (see: https://www.loc.gov/marc/bibliographic/bdsummary.html)
 data Marc21Subfield = Marc21Subfield {
       sfCode                   :: Marc21SubfieldCode,
       sfValue                  :: Marc21SubfieldValue
@@ -48,6 +52,8 @@ instance Show Marc21Subfield where
 type Marc21Tag = String
 type Marc21Indicator = Char
 
+-- |Represents a MARC21 Field (see: https://www.loc.gov/marc/bibliographic/bdsummary.html)
+-- Can be either a variable field or a control field.
 data Marc21Field = Marc21VariableField {
       fTag                     :: Marc21Tag,
       fIndicator1              :: Marc21Indicator,
@@ -63,10 +69,14 @@ instance Show Marc21Field where
     show (Marc21VariableField t i1 i2 sfs _) = t ++ " " ++ [i1] ++ " " ++ [i2] ++ "  " ++ (concatMap formatSubfield sfs)
     show (Marc21ControlField t _ val) = t ++ " " ++ " " ++ val
 
+-- |Returns a formatted MARC21 subfield.
+-- e.g. $a 7
 formatSubfield :: Marc21Subfield -> String
 formatSubfield subfield =
   --"$" ++ [(sfCode (subfields !! 0))] ++ (sfValue (subfields !! 0)) --this works
   "$" ++ [(sfCode subfield)] ++ " " ++ (sfValue subfield) ++ " "
+
+-- |Represents an entire MARC21 record containing Leader, raw string, Directory and list of Marc21Field
 data Marc21Record = Marc21Record {
       mrRawString                :: String,
       mrLeader                   :: Marc21Leader,
@@ -74,15 +84,17 @@ data Marc21Record = Marc21Record {
       mrFields                   :: [Marc21Field]
     } deriving (Show)
 
-
+-- |Returns only Marc21ControlField from the Marc21Record.
 getControlFields :: Marc21Record -> [Marc21Field]
 getControlFields record =
     filter (\field -> startswith "00" (fTag field)) (mrFields record)
 
+-- |Returns on Marc21VariableField from the Marc21Record.
 getVariableFields :: Marc21Record -> [Marc21Field]
 getVariableFields record =
     filter (\field -> not (startswith "00" (fTag field))) (mrFields record)
 
+-- |Reads a string and attempts to parse it into a Marc21Record.
 readFromString :: String -> Marc21Record
 readFromString s =
     Marc21Record recordString leader directory fields
@@ -94,6 +106,7 @@ readFromString s =
         directory = extractDirectory ((split [marc21FieldTerminator] s) !! 0)
         fields = extractFields s baseaddress directory
 
+-- |Reads a string and attempts to parse into a list of Marc21Record.
 readBatchFromString :: String -> [Marc21Record]
 readBatchFromString s =
     -- ignore last empty element from split
@@ -108,7 +121,7 @@ readAndPrint :: String -> String
 readAndPrint s =
     show (readFromString s)
 
-
+-- |Prints out a Marc21Record in the format returned by the yaz-marcdump utility (see: http://www.indexdata.com/yaz/doc/yaz-marcdump.html)
 marcDumpFormat :: Marc21Record -> String
 marcDumpFormat record =
     leader ++ "\n" ++ controlFields ++ "\n" ++ variableFields ++ "\n"
@@ -196,20 +209,33 @@ extractSubFields s =
           Marc21Subfield (str !! 0) (drop 1 str)
       subfields = map createSubfield rawSubfields
 
+-- |Returns Nothing or the Marc21Field from the given Marc21Record.
+-- * Example usage
+-- getField record "245"
 getField :: Marc21Record -> Marc21Tag -> Maybe Marc21Field
 getField record tag  =
   find (\field -> fTag field == tag) (mrFields record)
 
+-- |Returns Nothing or the Marc21Subfield from the given Marc21Record and Marc21Field.
+-- * Example usage
+-- getSubfield field 'a'
 getSubfield :: Marc21Field -> Marc21SubfieldCode -> Maybe Marc21Subfield
 getSubfield t sf =
   find (\subfield -> sfCode subfield  == sf) (fSubfields t)
 
+-- |Returns Marc21Field and Marc21Subfield from a given Marc21Record.
+-- * Example usage
+-- getFieldAndSubfield record "245" 'a'
 getFieldAndSubfield :: Marc21Record -> Marc21TagCode -> Marc21SubfieldCode -> Maybe Marc21SubfieldValue
 getFieldAndSubfield record tag subfieldcode = do
   field <- getField record tag
   subfield <- getSubfield field subfieldcode
   return $ sfValue subfield
 
+
+-- |Returns Bool indicating whether the given Marc21Tag is present in the Marc21Record.
+-- * Example usage
+-- hasField record "245"
 hasField :: Marc21Record -> Marc21Tag -> Bool
 hasField record tag =  tag `elem` (map fTag (mrFields record))
 
